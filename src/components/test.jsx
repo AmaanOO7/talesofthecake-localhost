5cdd45919d97329f6b0d529a26b0bbb38e6fb68a
AmaanOO7 <aks.aman.me@gmail.com>
2025-09-07 23:35:52 +0530

    Create test.jsx

---
+++ src/components/test.jsx
@@ -0,0 +1,7 @@
+export default function Test() {
+  return (
+    <div className="bg-red-500 text-white text-2xl p-6 fixed top-0 left-0 w-full z-50">
+      This should be FIXED at the top in red
+    </div>
+  );
+}
