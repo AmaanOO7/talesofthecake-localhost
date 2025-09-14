e14f2943ff734dd1cfb805ff893350bd57141528
AmaanOO7 <aks.aman.me@gmail.com>
2025-09-14 19:14:36 +0530

    Create Home.jsx

---
+++ src/components/Home.jsx
@@ -0,0 +1,13 @@
+// src/components/Home.jsx
+import React from 'react';
+
+function Home() {
+  return (
+    <div className="text-center py-20">
+      <h1 className="text-4xl font-bold">Welcome to Tales of the Cake</h1>
+      <p>Our amazing cakes are waiting for you!</p>
+    </div>
+  );
+}
+
+export default Home;
