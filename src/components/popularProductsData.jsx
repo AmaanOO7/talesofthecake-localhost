caead931a01dd5dd7a889b2aeefa8c0e8ec1202e
AmaanOO7 <aks.aman.me@gmail.com>
2025-09-18 15:35:02 +0530

    Update popularProductsData.jsx

--- src/components/popularProductsData.jsx
+++ src/components/popularProductsData.jsx
@@ -131,7 +131,7 @@ const POPULAR_PRODUCTS = [
     id: 22,
     name: "Almond cashew cookies",
     price: "₹1,300",
-    img: "https://drive.google.com/file/d/1leb8FbRqpOL_Q5XkPeUuEwRAr9Mb-rR6/view?usp=drive_link",
+    img: "https://drive.google.com/file/d/1leb8FbRqpOL_Q5XkPeUuEwRAr9Mb-rR6/view",
   },
 ];
 
