2094b74ccef9981aa8e6f9b9d9021e352c2c02bb
AmaanOO7 <aks.aman.me@gmail.com>
2025-09-18 13:15:02 +0530

    Create popularProductsData.jsx

---
+++ src/components/popularProductsData.jsx
@@ -0,0 +1,66 @@
+// src/popularProductsData.js
+
+const POPULAR_PRODUCTS = [
+  {
+    id: 1,
+    name: "Vanilla Story Cake",
+    price: "₹1,200",
+    img: "/images/choco-cake.jfif",
+  },
+  {
+    id: 2,
+    name: "Chocolate Chronicle",
+    price: "₹1,500",
+    img: "/images/pineapple-cake.jfif",
+  },
+  {
+    id: 3,
+    name: "Strawberry Bliss",
+    price: "₹1,300",
+    img: "/images/vanila-cake.jfif",
+  },
+  {
+    id: 4,
+    name: "Blueberry Heaven",
+    price: "₹1,400",
+    img: "/images/blueberry-cake.jfif",
+  },
+  {
+    id: 5,
+    name: "Lemon Delight",
+    price: "₹1,250",
+    img: "/images/lemon-cake.jfif",
+  },
+  {
+    id: 6,
+    name: "Red Velvet Charm",
+    price: "₹1,600",
+    img: "/images/red-velvet-cake.jfif",
+  },
+  {
+    id: 7,
+    name: "Coffee Crunch",
+    price: "₹1,350",
+    img: "/images/coffee-cake.jfif",
+  },
+  {
+    id: 8,
+    name: "Caramel Swirl",
+    price: "₹1,450",
+    img: "/images/caramel-cake.jfif",
+  },
+  {
+    id: 9,
+    name: "Pistachio Dream",
+    price: "₹1,550",
+    img: "/images/pistachio-cake.jfif",
+  },
+  {
+    id: 10,
+    name: "Mango Melody",
+    price: "₹1,300",
+    img: "/images/mango-cake.jfif",
+  },
+];
+
+export default POPULAR_PRODUCTS;
