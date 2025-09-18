51395a5a2aefd1a1418d26bfad540e6f7df397b7
AmaanOO7 <aks.aman.me@gmail.com>
2025-09-18 15:03:13 +0530

    Update productsData.jsx

--- src/components/productsData.jsx
+++ src/components/productsData.jsx
@@ -4,39 +4,39 @@
 const ALL_PRODUCTS = [
   {
     id: 1,
-    name: "Chocolate Delight",
-    price: "₹1200",
-    img: "https://picsum.photos/300/200?random=1",
+    name: "Bride to be Cake",
+    price: "₹2000",
+    img: "https://drive.google.com/file/d/17m59T7eGTxpdP7MIyapocgIaGAJ1Q-qP/view?usp=drive_link",
   },
   {
     id: 2,
-    name: "Vanilla Dream",
-    price: "₹1350",
-    img: "https://picsum.photos/300/200?random=2",
+    name: "Bento Cake",
+    price: "₹1850",
+    img: "https://drive.google.com/file/d/1umXoJeAa8jVfU2Q0t63urbaF5Ua8DU2X/view?usp=drive_link",
   },
   {
     id: 3,
-    name: "Strawberry Bliss",
+    name: "Calender Cake",
     price: "₹1500",
-    img: "https://picsum.photos/300/200?random=3",
+    img: "https://drive.google.com/file/d/1JyTLDsrPWAJ3XBqA-aCA9D-Etqe8SYnc/view?usp=drive_link",
   },
   {
     id: 4,
-    name: "Butterscotch Crunch",
+    name: "Chocolate cake",
     price: "₹1450",
-    img: "https://picsum.photos/300/200?random=4",
+    img: "https://drive.google.com/file/d/1sKoLxjZtS-yyuz1VDplG-3ifI8ccTfvl/view?usp=drive_link",
   },
   {
     id: 5,
-    name: "Pineapple Paradise",
+    name: "Panda Bento Cake",
     price: "₹1400",
-    img: "https://picsum.photos/300/200?random=5",
+    img: "https://drive.google.com/file/d/10kDblbwdamMUhLpis-qu4o83fewThuXG/view?usp=drive_link",
   },
   {
     id: 6,
-    name: "Black Forest Classic",
+    name: "Red Velvet Bento Cake",
     price: "₹1550",
-    img: "https://picsum.photos/300/200?random=6",
+    img: "https://drive.google.com/file/d/1cJeoC24owZ4W9o0TgZ-uv00QbiJscFkV/view?usp=drive_link",
   },
   // ✅ Add as many products as you like here
 ];
