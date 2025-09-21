c20ac49d9ebfd0237ba5691ecfabbe8f585b5479
AmaanOO7 <aks.aman.me@gmail.com>
2025-09-21 19:33:02 +0530

    Update popularProductsData.jsx

--- src/components/popularProductsData.jsx
+++ src/components/popularProductsData.jsx
@@ -1,42 +1,9 @@
-// src/popularProductsData.js
+// src/popularProductsData.jsx
+import ALL_PRODUCTS from "./productsData";
 
-const POPULAR_PRODUCTS = [
-  {
-    id: 1,
-    name: "Bride to be Cake",
-    price: "2000",
-    img: "https://raw.githubusercontent.com/AmaanOO7/Talesofthecake-images/7f7d16b370ed6c65b2acc26904c0de45ff3e01d0/Zomato%20pics/Bride%20to%20be%20Cake.jpg",
-  },
-  {
-    id: 2,
-    name: "Bento Cake",
-    price: "1850",
-    img: "https://raw.githubusercontent.com/AmaanOO7/Talesofthecake-images/7f7d16b370ed6c65b2acc26904c0de45ff3e01d0/Zomato%20pics/Bento%20Cake.jpg",
-  },
-  {
-    id: 3,
-    name: "Celandar Cake",
-    price: "1500",
-    img: "https://raw.githubusercontent.com/AmaanOO7/Talesofthecake-images/7f7d16b370ed6c65b2acc26904c0de45ff3e01d0/Zomato%20pics/Calender%20Cake.jpg",
-  },
-  {
-    id: 4,
-    name: "Chocolate Overloaded Cake",
-    price: "1450",
-    img: "https://raw.githubusercontent.com/AmaanOO7/Talesofthecake-images/7f7d16b370ed6c65b2acc26904c0de45ff3e01d0/Zomato%20pics/Chocolate%20Overloaded%20Cake.jpg",
-  },
-  {
-    id: 5,
-    name: "Panda Bento Cake",
-    price: "1400",
-    img: "https://raw.githubusercontent.com/AmaanOO7/Talesofthecake-images/7f7d16b370ed6c65b2acc26904c0de45ff3e01d0/Zomato%20pics/Panda%20Bento%20Cake.jpg",
-  },
-  {
-    id: 6,
-    name: "Red Velvet Bento Cake",
-    price: "1550",
-    img: "https://raw.githubusercontent.com/AmaanOO7/Talesofthecake-images/7f7d16b370ed6c65b2acc26904c0de45ff3e01d0/Zomato%20pics/Red%20Velvet%20Bento%20Cake.jpg",
-  },
-];
+// 👇 Pick specific product IDs to be "popular"
+const POPULAR_PRODUCTS = ALL_PRODUCTS.filter((p) =>
+  [5, 6, 15, 17].includes(p.id) // ✅ change IDs as needed
+);
 
 export default POPULAR_PRODUCTS;
